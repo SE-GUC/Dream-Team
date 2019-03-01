65eb0bd54344b4503ac4972715a32ca1510b1b76
MorcosBishay <morcos_bishay_888@hotmail.com>
2019-03-01 18:10:36 +0200

    Updated index.js file

--- DreamTeam_Project/index.js
+++ DreamTeam_Project/index.js
@@ -3,24 +3,24 @@ const mongoose = require('mongoose')
 
 // Require Router Handlers
 
-const reviewer = require('./route/api/reviewer')
-const Admin = require('./route/api/admin')
-const Investor = require('./route/api/investor')
-const Laywer = require('./route/api/lawyer')
-const form = require('./route/api/from')
-const lawyer_form = require('./route/api/lawyer_form')
-const reviewer_form = require('./route/api/reviewer_form')
-const company = require('./route/api/company')
+const reviewer = require('./routes/api/reviewer')
+const Admin = require('./routes/api/admin')
+const Investor = require('./routes/api/investor')
+const Laywer = require('./routes/api/lawyer')
+const form = require('./routes/api/form')
+const lawyer_form = require('./routes/api/lawyer_form')
+const reviewer_form = require('./routes/api/reviewer_form')
+const company = require('./routes/api/company')
 
 
 const app = express()
 
 // DB Config
-const db = require('./config/keys').mongoURI
+const db = require('./config/key').mongoURI
 
 // Connect to mongo
 mongoose
-    .connect('db')
+    .connect('mongodb+srv://dreamteam:dreamteampass@dreamteamdb19-dfqo6.mongodb.net/SumergeDB')
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err))
 
@@ -35,14 +35,14 @@ app.get('/', (req,res) => res.send(`<h1>Person</h1>`))
 
 // Direct to Route Handlers
 
-app.use('/api/reviewer', reviewer)
+/*app.use('/api/reviewer', reviewer)
 app.use('/api/admin',Admin)
 app.use('/api/lawyer',Laywer)
 app.use('/api/investor',Investor)
 app.use('/api/form',form)
 app.use('/api/lawyer_form',lawyer_form)
 app.use('/api/reviewer_form',reviewer_form)
-app.use('/api/company',company)
+app.use('/api/company',company)*/
 
 
 
@@ -53,7 +53,7 @@ app.use((req,res) => res.status(404).send(`<h1>Can not find what you're looking 
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server on ${port}`))
-Collapse
+//Collapse
 
 
 
