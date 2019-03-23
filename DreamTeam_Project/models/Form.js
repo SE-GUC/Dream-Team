65156e448758350a165f4ac5b08cf69324c71bfa
benoanwar <beno12.97@gmail.com>
2019-03-23 17:32:29 +0200

    models Form

--- DreamTeam_Project/models/Form.js
+++ DreamTeam_Project/models/Form.js
@@ -1,138 +1,109 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const BoardofdirectorsSchema= require('../models/Boardofdirectors');
+const BoardofdirectorsSchema = require('../models/Boardofdirectors');
 const FormSchema = new Schema({
- companyName: {
-   type: String,
-   required: true
- },
- companyNameEng: {
-   type: String,
-   required: false
- },
- companyType: {
-   type: String,
-   required: false
- },
- headquarters: {
-   governorate: {
-     type: String,
-     required: true
-   },
-   city: {
-     type: String,
-     required: true
-   },
-   address: {
-     type: String,
-     required: true
-   },
-   telephone: {
-     type: String,
-     required: true
-   },
-   fax: {
-     type: String,
-     required: true
-   }
- },
+  companyName: {
+    type: String,
+    required: true
+  },
 
- // boardOfDirectors: {
- //       name: {
- //       type: String,
- //       required: true
- //       } ,
- //       investorType  :{
- //       type: String,
- //       require: true
- //       } ,
- //       gender: {
- //         type: String,
- //         required: true
- //       } ,
- //       Nationality: {
- //         type: String,
- //         required: true
- //       },
- //       typeID: {
- //         type: String,
- //         required: true
- //        },
- //       numberID: {
- //         type: Number,
- //         required: true
- //       },
- //       dateOfBirth: {
- //         type: Date,
- //         required: true
- //       },
- //       titleinBoard: {
- //         type: String,
- //         required: true
- //       }
+  companyNameEng: {
+    type: String,
+    required: false
+  },
 
- // },
+  companyType: {
+    type: String,
+    required: false
+  },
 
+  headquarters: {
+    governorate: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    address: {
+      type: String,
+      required: true
+    },
+    telephone: {
+      type: String,
+      required: true
+    },
+    fax: {
+      type: String,
+      required: true
+    }
+  },
 
- financialInfo: {
-   Currency: {
-     type: String
+  financialInfo: {
+    Currency: {
+      type: String
+    },
+    Capital: {
+      type: Number
+    }
+  },
 
-   },
-   Capital: {
-     type: Number
+  investor: {
+    type: Schema.Types.ObjectId,
+    ref: 'Investor',
+    required: true
+  },
 
-   }
- },
- investor: {
-   type: Schema.Types.ObjectId,
-   ref: 'Investor',
-   required: true
- },
- lawyer: {
-   type: Schema.Types.ObjectId,
-   ref: 'Lawyer'
- },
- lawyerComment: {
-   type: String,
-   required: false
- },
+  lawyer: {
+    type: Schema.Types.ObjectId,
+    ref: 'Lawyer'
+  },
 
- lawyerDecision: {
-   type: Number,
-   required: false
- },
- reviewer: {
-   type: mongoose.Schema.Types.ObjectId,
-   ref: 'Reviewer'
+  lawyerComment: {
+    type: String,
+    required: false
+  },
 
- },
- reviwerComment: {
-   type: String
+  lawyerDecision: {
+    type: Number,
+    required: false
+  },
 
- },
- reviewerDecision: {
-   type: Number
+  reviewer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Reviewer'
+  },
 
- },
- dateOfApproval: {
-   type: Date
+  reviwerComment: {
+    type: String
+  },
 
- },
- amountOfPayment: {
-   type: Double
+  reviewerDecision: {
+    type: Number
+  },
 
- },
- dateOfPayment: {
-   type: String
- },
- paymentId: {
-   type: Number
- },
- formStatus: {
-   type: String
- },
+  dateOfApproval: {
+    type: Date
+  },
 
- board:[BoardofdirectorsSchema]
+  amountOfPayment: {
+    type: Double
+  },
+
+  dateOfPayment: {
+    type: String
+  },
+
+  paymentId: {
+    type: Number
+  },
+  
+  formStatus: {
+    type: String
+  },
+
+  board: [BoardofdirectorsSchema]
+  
 })
 module.exports = Form = mongoose.model('Form', FormSchema);
