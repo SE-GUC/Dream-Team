3c18cc20460bd19de2f1e5f8ea21185fa9dc3ffc
benoanwar <beno12.97@gmail.com>
2019-03-23 19:20:48 +0200

    bug fixes

--- DreamTeam_Project/index.js
+++ DreamTeam_Project/index.js
@@ -3,15 +3,8 @@ const mongoose = require('mongoose')
 //const router = express.Router()
 
 // Require Router Handlers
-
-const reviewer = require('./routes/api/reviewer')
-const Admin = require('./routes/api/admin')
-const Investor = require('./routes/api/investor')
-const Laywer = require('./routes/api/lawyer')
 const form = require('./routes/api/form')
-const lawyer_form = require('./routes/api/lawyer_form')
-const reviewer_form = require('./routes/api/reviewer_form')
-const company = require('./routes/api/company')
+const user = require('./routes/api/user')
 
 
 const app = express()
@@ -21,10 +14,10 @@ const db = require('./config/key').mongoURI
 
 // Connect to mongo
 mongoose
-    .connect('mongodb+srv://dreamteam:dreamteampass@dreamteamdb19-dfqo6.mongodb.net/SumergeDB')
+//process.env.mongouri
+    .connect( db)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err))
-
 // Init middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
@@ -36,24 +29,16 @@ app.get('/', (req,res) => res.send(`<h1>Person</h1>`))
 
 // Direct to Route Handlers
 
-app.use('/api/reviewer', reviewer)
-app.use('/api/admin',Admin)
-app.use('/api/lawyer',Laywer)
-app.use('/api/investor',Investor)
+app.use('/api/user', user)
 app.use('/api/form',form)
-app.use('/api/lawyer_form',lawyer_form)
-app.use('/api/reviewer_form',reviewer_form)
-app.use('/api/company',company)
-
-
-
-
-
 
 app.use((req,res) => res.status(404).send(`<h1>Can not find what you're looking for</h1>`))
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Server on ${port}`))
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 //Collapse
 
 
