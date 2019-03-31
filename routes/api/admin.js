58d63b28d9f34d815a57311bd9696558b13b1725
DaliaMedhat <Dalia.dawoud@student.guc.edu.eg>
2019-03-31 17:23:00 +0200

    Rearrangment of routes for authoraization

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -1,14 +1,198 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/User')
+const userValidator = require('../../validations/userValidations')
+const formValidator = require('../../validations/formValidations')
+const bcrypt = require('bcryptjs')
 const User = require("../../models/User")
 const Form = require('../../models/Form')
+const typesEnum = require ('../../enums/accountType')
 const formEnum=require('../../enums/formStatus')
-const validator = require('../../validations/formValidations')
-const typesEnum=require('../../enums/accountType')
+const entity= require('../../enums/entityType')
+const formType=require('../../enums/formType')
+const regulatedLaw=require('../../enums/regulatedLaw')
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+// configuration option that tells the parser to use the classic encoding 
+router.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+//3.6-Admin view all lawyers  
+router.get('/admin/getLawyer', async (req, res) => {
+    const users = await User.find({"accountType": "lawyer"})
+    res.json({
+        data: users
+    })
+})
 
 
+//3.4-Admin view all investors
+router.get('/admin/viewinvestor', async (req, res) => {
+    const users = await User.find({"accountType": "investor"})
+    res.json({
+        data: users
+    })
+})
+
+//3.7-As an admin I should be able to view all reviewers
+router.get('/admin/getReviewer', async (req, res) => {
+    const users = await User.find({"accountType": "reviewer"})
+    res.json({
+        data: users
+    })
+})
 
 
+//Admin approving an account
+router.put("/admin/approve/:id", async (req, res) => {
+    try {
+        const id = req.params.id
+        const user = await User.findById(id);
+        if (!user)
+            return res.status(404).send({
+                error: "This User does not exist"
+            });
+        const approve={
+            accountStatus: true
+        }
+        const approvedacc = await User.findByIdAndUpdate(id,approve)
+        const acc= await User.findById(id);
+
+        res.json({
+            msg: "account have been approved",
+            data: acc
+    })
+    } catch (err) {
+        res.json({
+            msg: err.message
+        });
+    }
+});
+
+
+//admin viewing all entity employees
+router.get("/admin/employee", async (req, res) => {
+    try{
+ //   const admin = await User.find({"accountType": "admin"})
+   const users= await User.find({accountType: {$ne: typesEnum.accountTypes.INVESTOR}})
+ 
+    res.json({
+ 
+       data: users
+ 
+    })
+}
+catch(err){
+    res.json({
+        msg: err.message
+    })
+}
+ 
+ })
+
+//As an Admin I should be able to Reject account
+router.put("/admin/reject/:id", async (req, res) => {
+    try {
+        const id = req.params.id
+        const user = await User.findById(id);
+        if (!user)
+            return res.status(404).send({
+                error: "This User does not exist"
+            });
+        const approve={
+            accountStatus: false
+        }
+        const approvedacc = await User.findByIdAndUpdate(id,approve);
+        const acc= await User.findById(id);
+        res.json({
+                msg: "account have been disapproved",
+                data: acc
+        })
+    } catch (err) {
+        res.json({
+            msg: err.message
+        });
+    }
+});
+
+//DELETE USER BY ID
+router.delete("/:id", async (req, res) => {
+    try {
+        const id = req.params.id
+        const deletedUser = await User.findByIdAndDelete(id);
+        res.json({
+            msg: "The User has been deleted successfully",
+            data: deletedUser
+        })
+    } catch (error) {
+        res.json({
+            msg: error.message
+        });
+    }
+})
+
+//As an Admin I should be able to send message to rejected accounts
+router.put('/admin/sendRejectionMsg/:id', async (req,res) => {
+    try {
+     const id = req.params.id
+     const user = await User.find({accountStatus:false},{_id:id} );
+     if(!user) return res.status(404).send({error: 'user does not exist'})
+     const updateduser = await User.findByIdAndUpdate(id,req.body)
+     res.json({msg: 'user updated successfully'})
+    }
+    catch(error) {
+        // We will be handling the error later
+        console.log(error)
+    }
+ 
+  })
+
+//view all users
+router.get("/getUsers", async (req, res) => {
+    const users = await User.find();
+    res.json({ data: users });
+  });
+
+//get user by user id 
+router.get("/getUsers/:id", async (req, res) => {
+    try {
+        const id = req.params.id
+        const user = await User.findById(id);
+        if (!user)
+            return res.status(404).send({
+                error: "This User does not exist"
+            });
+        res.json({
+            data: user
+        });
+    } catch (err) {
+        res.json({
+            msg: err.message
+        }   );
+    }
+});
+
+//As a Lawyer I should view all forms that I have approved/rejected
+router.get("/lawyer/:id", async (req, res) => 
+{
+   const id = req.params.id
+   const user = await User.findById(id);
+       if (!user)
+           return res.status(404).send({
+               error: "This User does not exist"
+           })
+    // const lawyerForm1 = await Form.findById(id,{"lawyerDecision": 1}) 
+    // const lawyerForm2 = await Form.findById(id, {"lawyerDecision": -1 })
+    const lawyers = await Form.find({"lawyer":id})       
+  
+       res.json({ data: lawyers })
+    
+})
 
 module.exports = router
--- routes/api/internalPortal.js
+++ routes/api/internalPortal.js
@@ -1,14 +1,122 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/User')
+const userValidator = require('../../validations/userValidations')
+const formValidator = require('../../validations/formValidations')
+const bcrypt = require('bcryptjs')
 const User = require("../../models/User")
 const Form = require('../../models/Form')
+const typesEnum = require ('../../enums/accountType')
 const formEnum=require('../../enums/formStatus')
-const validator = require('../../validations/formValidations')
-const typesEnum=require('../../enums/accountType')
+const entity= require('../../enums/entityType')
+const formType=require('../../enums/formType')
+const regulatedLaw=require('../../enums/regulatedLaw')
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+// configuration option that tells the parser to use the classic encoding 
+router.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+//I should be able to read all Forms
+router.get('/', async (req,res) => {
+    const form = await Form.find()
+    res.json({data: form})
+})
 
 
 
+   // I should be able to Read forms by ID 
+   router.get("/:id", async (req, res) => {
+    try {
+        const id = req.params.id
+        const form = await Form.findById(id);
+        if (!form)
+            return res.status(404).send({
+                error: "This Form does not exist"
+            });
+        res.json({
+            data: form
+        });
+    } catch (err) {
+        res.json({
+            msg: err.message
+        });
+    }
+  })
+  
+  //I should be able to get undecided forms for lawyer or reviwer
+router.get('/undecidedForms/:loggedintype', async (req, res) => {
+    const loggedintype = req.params.loggedintype
+   
+    if(loggedintype===typesEnum.accountTypes.LAWYER){
+      var forms = await Form.find({"lawyerDecision": 0 })
+      res.json({
+        data: forms
+      })
+    }
+    else if(loggedintype===typesEnum.accountTypes.REVIEWER){
+      var forms = await Form.find({"reviewerDecision": 0 })
+      res.json({
+        data: forms
+      })
+    }
+   
+   
+   
+   else return res.status(404).send({
+    error: "you are not allowed to perform the requested operation"
+   })})
+   
+   //As a lawyer or reviewer or admin I should be able to get form status for any or specific investor to track his forms
+   router.get("/formStatus/:loggedintype/:id", async (req, res) => {
+    const loggedintype = req.params.loggedintype
+    const id = req.params.id
+   
+    if(loggedintype===typesEnum.accountTypes.INVESTOR){
+      var forms = await Form.find({"investor": id })
+      res.json({
+        data: forms
+      })
+    }
+    else if(loggedintype===typesEnum.accountTypes.REVIEWER||loggedintype===typesEnum.accountTypes.LAWYER||loggedintype===typesEnum.accountTypes.ADMIN){
+      const forms = await Form.find();
+      res.json({
+        data: forms
+      })
+    }
+    else return res.status(404).send({
+      error: "you are not allowed to perform the requested operation"
+    })
+   
+   });
+   
+
+//As an investor i should be able to view my pending companies
+ router.get('/pending/:id', async (req, res) => {
+    const id = req.params.id
+    const forms =await Form.find({investor:id, formStatus: {$ne:formEnum.formStatus.APPROVED}})
+       res.json({
+   data: forms
+})
+})
+
+//As an Investor i should be able to view my running companies
+router.get('/running/:id', async (req, res) => {
+
+const id = req.params.id
+const forms= await Form.find({investor:id, formStatus:formEnum.formStatus.APPROVED })
+  res.json({
+    data: forms
+})
+
+
+})
 
 
 module.exports = router
