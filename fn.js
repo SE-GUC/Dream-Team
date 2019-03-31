f85daae8a4306e3a95ba4f35f566115ef946774b
MNakhla <maria.nakhla@student.guc.edu.eg>
2019-03-31 21:41:13 +0200

    [SP3_7.1] search for specific detail in forms, [unitTests for SP2_7.1,7.2]

--- fn.js
+++ fn.js
@@ -7,7 +7,17 @@ const functions = {
     insert: async (user) => {
         const books = await axios.post('http://localhost:3000/api/user/createUser', user);
         return books;
-    }
+    },
 
+    userStatus: async () => {
+        const userStatus = await axios.get('http://localhost:3000/api/internalPortal/formStatus/investor/5c9d8481cf00d53f74c3737c')
+        console.log(userStatus)
+        return userStatus
+    },
+    LRundecidedForms: async () => {
+        const LRundecidedForms = await axios.get('http://localhost:3000/api/internalPortal/undecidedForms/lawyer')
+        console.log(LRundecidedForms)
+        return LRundecidedForms
+    }
 };
 module.exports = functions;
--- routes/api/form.js
+++ routes/api/form.js
@@ -7,6 +7,36 @@ const formEnum=require('../../enums/formStatus')
 const validator = require('../../validations/formValidations')
 const typesEnum=require('../../enums/accountType')
 
+router.get('/search',async (req,res) =>{
+ 
+    const search = await Form.find(req.body)
 
+  res.json({
+    data: search
+  })
+})
+
+//As a lawyer or reviewer or admin I should be able to get form status for any or specific investor to track his forms
+router.get("/formStatus/:loggedintype/:id", async (req, res) => {
+    const loggedintype = req.params.loggedintype
+    const id = req.params.id
+   
+    if(loggedintype===typesEnum.accountTypes.INVESTOR){
+      var forms = await Form.find({"investor": id })
+      res.json({
+        data: forms
+      })
+    }
+    else if(loggedintype===typesEnum.accountTypes.REVIEWER||loggedintype===typesEnum.accountTypes.LAWYER||loggedintype===typesEnum.accountTypes.ADMIN){
+      const forms = await Form.find();
+      res.json({
+        data: forms
+      })
+    }
+    else return res.status(404).send({
+      error: "you are not allowed to perform the requested operation"
+    })
+   
+   });
 
 module.exports = router
