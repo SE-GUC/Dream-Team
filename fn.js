8fda3859b62b795b48d8fe17c3109e66f6c25b97
aya.bedewi@student.guc.edu.eg <aya.bedewi@student.guc.edu.eg>
2019-03-31 23:01:42 +0200

    formValidations + test for stories view investor/reviewer and track request as well as view amount and due date/ view finalized cases

--- fn.js
+++ fn.js
@@ -1,13 +1,46 @@
 const axios = require('axios');
 const functions = {
-    getUser: async () => {
-        const books = await axios.get('http://localhost:3000/api/user/getUsers/')
-        return books
+
+    notifyAmountAndDueDate:  async() => {
+        const viewin = await axios.get('http://localhost:3000/api/investor/notifyAmountAndDueDate/:id')
+        return viewin
+},
+
+viewAllInvestors: async () => {
+const getInvestor =await axios.get('http://localhost:3000/api/admin/viewinvestor')
+return getInvestor
+},
+
+
+
+viewAllReviewers: async () => {
+    const getRev =await axios.get('http://localhost:3000/api/admin/getReviewer')
+    return getRev
     },
-    insert: async (user) => {
-        const books = await axios.post('http://localhost:3000/api/user/createUser', user);
-        return books;
-    }
+
+investortrack: async () => {
+    const getInvestor =await axios.get('http://localhost:3000/api/investor/trackRequest/5ca10eadb7701f2158efbad2')
+    return getInvestor
+    },
+
+
+    invUpdateForm: async () => {
+        const getInvestor =await axios.put('http://localhost:3000/api/investor/updateForm/5ca11184911834593cc03510/5ca10eadb7701f2158efbad2')
+        return getInvestor
+        },
+
+
+        notifyAmountAndDueDate: async () => {
+            const getInvestor =await axios.get('http://localhost:3000/api/investor/notifyAmountAndDueDate/:id')
+            return getInvestor
+            },
+            
+
+            finializedCases: async () => {
+                const getlawyer =await axios.get('http://localhost:3000/api/lawyer/5ca10eadb7701f2158efbad2')
+                return getlawyer
+            }
+
 
 };
 module.exports = functions;
--- test.js
+++ test.js
@@ -5,9 +5,9 @@ const User = require('./models/User')
 // test('adds 1 + 2 to be 3', () => {
 //   expect(funcs.add(1, 2)).toBe(3);
 // });
-afterEach(() => {
+// afterEach(() => {
   
-})
+// })
 // aftereach(()=>{
 
 // })
@@ -65,36 +65,102 @@ afterEach(() => {
 //     expect(response.data.data.length).toBe(11)
 //   });
 
-let user1 = {
-  accountType: "admin",
-  name: "Schroeder",
-  gender: "female",
-  nationality: "mangenese",
-  typeID: "other id",
-  numberID: 847372204,
-  dateOfBirth: "Tue Jul 10 1979 10:43:17 GMT+0000 (UTC)",
-  address: "115 Varick Street, Talpa, Tennessee, 2087",
-  phoneNumber: "15",
-  faxNumber: "15",
-  email: "schrogggerg@undefined.me",
-  password: "Racdkjbhael",
-  capital: 6369,
-  capitalCurrency: "USD",
-  accountStatus:"true",
-  investorType: "SSC"
-};
-  it(`User's name should be vegerger`,async () => {
-    // // expect.assertions(1)
+// let user1 = {
+//   accountType: "admin",
+//   name: "Schroeder",
+//   gender: "female",
+//   nationality: "mangenese",
+//   typeID: "other id",
+//   numberID: 847372204,
+//   dateOfBirth: "Tue Jul 10 1979 10:43:17 GMT+0000 (UTC)",
+//   address: "115 Varick Street, Talpa, Tennessee, 2087",
+//   phoneNumber: "15",
+//   faxNumber: "15",
+//   email: "schrogggerg@undefined.me",
+//   password: "Racdkjbhael",
+//   capital: 6369,
+//   capitalCurrency: "USD",
+//   accountStatus:"true",
+//   investorType: "SSC"
+// };
+//   it(`User's name should be vegerger`,async () => {
+//     // // expect.assertions(1)
    
-    const user =  await funcs.insert(user1)
-    // const user1= user.bodyParser;
-    expect(user.data.data.name).toEqual('Schroeder')
-    // expect(user.data.data.length).toBeGreaterThan(0)
-  });
-  // test(`User's name should be vegerger`,async () => {
+//     const user =  await funcs.insert(user1)
+//     // const user1= user.bodyParser;
+//     expect(user.data.data.name).toEqual('Schroeder')
+//     // expect(user.data.data.length).toBeGreaterThan(0)
+//   });
+//   // test(`User's name should be vegerger`,async () => {
   //   expect.assertions(2)
   //   const user =  await funcs.getUser()
   //   // const user1= user.bodyParser;
   //   expect(user.data.data[0].name).toEqual('Schroeder')
   //   expect(user.data.data.length).toBeGreaterThan(0)
   // });
+
+
+   //  tested
+   test(`view all investors `,async () => {
+    expect.assertions(1)
+      const del =  await funcs.viewAllInvestors()
+       // const user1= user.bodyParser;
+      //  console.log("This is the field "+del)
+      // expect().toBeGreaterThanOrEqual(404)
+      expect(del.data.data[0].name).toEqual('Schroeder')
+      }),
+    
+
+         // tested
+  test(`view all Reviewers `,async () => {
+    expect.assertions(1)
+      const del =  await funcs.viewAllReviewers()
+       // const user1= user.bodyParser;
+      //  console.log("This is the field "+del)
+      // expect().toBeGreaterThanOrEqual(404)
+      expect(del.data.data[0].name).toEqual('ayaelreviewerkedakeda')
+      }),
+     
+  //  //TESTED
+  test(`investor Tracking by _id 5c9fe6036a36eb47ec6db285 `,async () => {
+    expect.assertions(1)
+      const del =  await funcs.investortrack()
+       // const user1= user.bodyParser;
+      //  console.log("This is the field "+del)
+      // expect().toBeGreaterThanOrEqual(404)
+      expect(del.data.data[0].investor).toEqual('5ca10eadb7701f2158efbad2')
+      })
+
+
+      test(`investor Tracking by _id 5c9fe6036a36eb47ec6db285 `,async () => {
+        expect.assertions(1)
+          const del =  await funcs.investortrack()
+           // const user1= user.bodyParser;
+          //  console.log("This is the field "+del)
+          // expect().toBeGreaterThanOrEqual(404)
+          expect(del.data.data[0].investor).toEqual('5ca10eadb7701f2158efbad2')
+          })
+
+  
+          
+
+          test(`notify Amount And DueDate`  ,async () => {
+            expect.assertions(1)
+            const fees =  await funcs.notifyAmountAndDueDate()
+            expect(fees.data.data[0].dateOfPayment).toBeDefined()
+            });
+
+        test(`finalized cases`  ,async () => {
+              expect.assertions(1)
+              const fees =  await funcs.finializedCases()
+              expect(fees.data.data[0].lawyerDecision).toEqual('1')
+              });
+              
+              test(`inv Update Form`  ,async () => {
+                expect.assertions(1)
+                const x =  await funcs.invUpdateForm()
+                expect(x.data.msg).toEqual('Form updated successfully')
+              })
+
+
+              
