430df7a631ade83af14fbf5d609186647dd075dd
MorcosBishay <morcos_bishay_888@hotmail.com>
2019-03-31 15:55:36 +0200

    added route files

---
+++ routes/api/investor.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const User = require("../../models/User")
+const Form = require('../../models/Form')
+const formEnum=require('../../enums/formStatus')
+const validator = require('../../validations/formValidations')
+const typesEnum=require('../../enums/accountType')
+
+
+
+
+module.exports = router
