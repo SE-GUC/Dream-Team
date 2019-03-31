373cdca8751e7434360fbeba1265a6bfd3550837
Antony Waseem Ayoub <47082495+antonywaseem@users.noreply.github.com>
2019-04-01 00:57:41 +0200

    Urgent FIX

--- config/key_prod.js
+++ config/key_prod.js
@@ -1,6 +1,7 @@
 module.exports = {
 
-    mongoURI: process.env.MONGO_URI
+   mongoURI: 'mongodb+srv://dreamteam:dreamteampass@dreamteamdb19-dfqo6.mongodb.net/SumergeDB',
+secretOrKey: 'verysecretkey'
 }
 
 
