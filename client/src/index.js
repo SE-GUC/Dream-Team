2245e8bf320574be42a6bc2dc465fc37a74980a6
rawanbadr <rawan.badr@student.guc.edu.eg>
2019-04-07 21:14:18 +0200

    added 2 routes path

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import HomePage from './pages/homepage';
-import Login from './components/login';
-import UpdateTest from './components/updateTest';
-import EmployeeTable from './components/employeeTable';
-import SignUp from './components/signup';
-import FormTable from './components/formTable';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./pages/homepage";
+import Login from "./components/login";
+import UpdateTest from "./components/updateTest";
+import EmployeeTable from "./components/employeeTable";
+import SignUp from "./components/signup";
+import FormTable from "./components/formTable";
 
-import UserTable from './components/userTable/userTable';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Case from './pages/LRspecificCase';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import UserTable from "./components/userTable/userTable";
+import ComRule from "./components/comRule/comRule";
+import CalcRule from "./components/calcRule/calcRule";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Case from "./pages/LRspecificCase";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <Router>
@@ -25,7 +27,9 @@ ReactDOM.render(
       <Route path="/employeeTable" component={EmployeeTable} />
       <Route path="/formTable" component={FormTable} />
       <Route path="/userTable" component={UserTable} />
+      <Route path="/companyRules" component={ComRule} />
+      <Route path="/feesCalculation" component={CalcRule} />
     </div>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
