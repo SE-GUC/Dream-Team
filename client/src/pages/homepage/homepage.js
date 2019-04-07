5c58f9c9c3248e19a3d502e86dca33a8e152f5f4
benoanwar <beno12.97@gmail.com>
2019-04-07 21:54:54 +0200

    new branch

--- client/src/pages/homepage/homepage.js
+++ client/src/pages/homepage/homepage.js
@@ -1,36 +1,16 @@
 import React, { Component } from 'react';
 import '../homepage/homepage.css';
-import Login from '../../components/login'
+import Login from '../../components/login';
 // import Table from '../../components/userTable'
-import Tableform from '../../components/formTable'
+import Tableform from '../../components/formTable';
 class HomePage extends Component {
-  // state = {
-    
-  // };
-
-  // componentDidMount() {
-  //   this.callApi()
-  //     .then(res => this.setState({ response: res.data[0].name }))
-  //     .catch(err => console.log(err));
-  // }
-
-  // callApi = async () => {
-  //   const response = await fetch('/api/user/getUsers');
-  //   const body = await response.json();
-
-  //   if (response.status !== 200) throw Error(body.message);
-
-  //   return body;
-  // };
- 
   render() {
     return (
       <div className="HomePage">
-       
         IN HomePage.JS
-        <Login/>
+        <Login />
         {/* <Table/> */}
-        <Tableform/>
+        <Tableform />
       </div>
     );
   }
