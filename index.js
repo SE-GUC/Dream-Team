215348affae5fe7659079fc004c9d4c109d69c53
Morcos Adly Halim <47229671+MorcosBishay@users.noreply.github.com>
2019-04-08 01:37:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,25 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 5000;
 }
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
+
 app.listen(port);
 //Collapse
 
