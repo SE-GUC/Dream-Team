6673ef9e2c283173791b21d0de68d18d38ca47f1
rawanbadr <rawan.badr@student.guc.edu.eg>
2019-04-07 21:13:42 +0200

    navigation bar

--- client/src/components/layout/layout.js
+++ client/src/components/layout/layout.js
@@ -7,12 +7,18 @@ class Layout extends Component {
     return (
       <div>
         <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="layout">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#home" to="/">
+              Home
+            </Nav.Link>
             <Nav.Link href="#companies">Companies</Nav.Link>
-            <Nav.Link href="#rules">Companies Rules</Nav.Link>
-            <Nav.Link href="#feesRules">Calculation Fees Rules</Nav.Link>
+            <Nav.Link href="/companyRules" to="/companyRules">
+              Companies Rules
+            </Nav.Link>
+            <Nav.Link href="/feesCalculation" to="/feesCalculation">
+              Calculation Fees Rules
+            </Nav.Link>
           </Nav>
         </Navbar>
       </div>
