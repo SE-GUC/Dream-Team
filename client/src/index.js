5698f93ecfa830bf7008f71a7e488c38710c5558
DaliaMedhat <Dalia.dawoud@student.guc.edu.eg>
2019-04-07 17:24:43 +0200

    Redirecting through router is done to reach investor's page instead of homepage display

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import HomePage from './pages/homepage';
-import Case from './pages/LRspecificCase';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./pages/homepage";
+import Login from "./components/login";
 
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<HomePage />, document.getElementById('root'));
-// ReactDOM.render(<Case />, document.getElementById('root'));
+import "bootstrap/dist/css/bootstrap.css";
+import Investor from "./components/investor";
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/login" component={Login} />
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      <Route path="/investor" component={Investor} />
+    </div>
+  </Router>,
+  document.getElementById("root")
+);
--- client/src/pages/homepage/homepage.js
+++ client/src/pages/homepage/homepage.js
@@ -1,33 +1,64 @@
 import React, { Component } from "react";
 import "../homepage/homepage.css";
-import Login from "../../components/login";
 
-// import SignUp from "../../components/signup";
-// import Table from "../../components/userTable";
-// import Tableform from "../../components/formTable";
-// import AuthHelperMethods from "./components/AuthHelperMethods";
-// import withAuth from "./components/withAuth";
-import Investorr from "../../components/investor";
 class HomePage extends Component {
   state = {};
-
-  // Auth = new AuthHelperMethods();
+  update() {
+    this.props.history.push("/update");
+  }
+  viewInvestor() {
+    this.props.history.push("/investor");
+  }
+  login() {
+    this.props.history.push("/login");
+  }
+  signUp() {
+    this.props.history.push("/signup");
+  }
+  userTable() {
+    this.props.history.push("/userTable");
+  }
+  employeeTable() {
+    this.props.history.push("/employeeTable");
+  }
+  formTable() {
+    this.props.history.push("/formTable");
+  }
 
   _handleLogout = () => {
     this.Auth.logout();
-
-    this.props.history.replace("/api/login");
+    this.props.history.replace("/login");
   };
 
   render() {
     return (
-      <div className="HomePage">
-        IN HomePage.JS
-        <Login />
-        <h1>Investorr Only(add your ID to check cases)</h1>
-        <Investorr />
-        {/* <Table ApiURL="api/user/getUsers" /> */}
-        {/* <Tableform /> */}
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome Home</h2>
+        </div>
+        <div>
+          To Login Press here
+          <button
+            className="btn btn-primary width-150"
+            onClick={e => {
+              this.login();
+            }}
+          >
+            Click to login
+          </button>
+        </div>
+
+        <div>
+          To view Pending and Approved companies
+          <button
+            className="btn btn-primary width-150"
+            onClick={e => {
+              this.viewInvestor();
+            }}
+          >
+            Click to view Pending and Approved companies
+          </button>
+        </div>
       </div>
     );
   }
