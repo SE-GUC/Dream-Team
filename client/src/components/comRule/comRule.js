7f5609ad1096683bb6a0f8c6f734d299f8d3edc2
rawanbadr <rawan.badr@student.guc.edu.eg>
2019-04-12 04:44:51 +0200

    gwt from data base the rules

--- client/src/components/comRule/comRule.js
+++ client/src/components/comRule/comRule.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
 // import { Table } from "reactstrap";
 class comRule extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: []
+    };
+  }
+  componentDidMount() {
+    fetch("api/user/companyRules/")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          response: json.data
+        });
+      });
+  }
+
   render() {
-    return;
+    return <p>{this.state.response}</p>;
   }
 }
 export default comRule;
