f5b7920137533ba23260ad5cdd73edc2f04ea783
MorcosBishay <morcos_bishay_888@hotmail.com>
2019-04-14 15:07:35 +0200

    test

--- client/src/index.js
+++ client/src/index.js
@@ -6,32 +6,31 @@ import UpdateTest from "./components/updateTest";
 import EmployeeTable from "./components/employeeTable";
 import SignUp from "./components/signup";
 import FormTable from "./components/formTable";
-import HomePage from './pages/homepage';
+import HomePage from "./pages/homepage";
 // import Login from './components/login';
 // import UpdateTest from './components/updateTest';
 // import EmployeeTable from './components/employeeTable';
 // import SignUp from './components/signup';
 // import FormTable from './components/formTable';
-import searchBar from './components/searchBar';
+import searchBar from "./components/searchBar";
 
-import UserTable from './components/usertable/userTable';
-import LawyerViewhisCases from './components/LawyerViewhisCases'
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Case from './pages/LRspecificCase';
-import 'bootstrap/dist/css/bootstrap.css';
-import ReviewerViewhisForms from './components/ReviewerViewhisForms'
+import UserTable from "./components/userTable/userTable";
+import LawyerViewhisCases from "./components/LawyerViewhisCases";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Case from "./pages/LRspecificCase";
+import "bootstrap/dist/css/bootstrap.css";
+import ReviewerViewhisForms from "./components/ReviewerViewhisForms";
 // import UserTable from "./components/userTable/userTable";
 // import * as serviceWorker from "./serviceWorker";
 import Investor from "./components/investor";
-import ComRule from "./components/comRule/comRule"
+import ComRule from "./components/comRule/comRule";
 import CalcRule from "./components/calcRule/calcRule";
-
 
 ReactDOM.render(
   <Router>
     <div>
-    <Route exactpath="/" component={HomePage} />
+      <Route exactpath="/" component={HomePage} />
       <Route path="/login" component={Login} />
       <Route path="/investor" component={Investor} />
       <Route path="/signup" component={SignUp} />
